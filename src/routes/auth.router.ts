c1071c7e9a28b8e402d637860e7e69794a651d19
KonMam <k.mamonas@gmail.com>
2022-09-05 21:40:34 +0300

    auth route refactoring

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,19 +1,18 @@
 import express from 'express'
-import { appDataSource } from '../data-source';
-import { User } from '../entities/user.entity';
-import { Secret, sign, verify } from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import { Secret, sign, verify } from 'jsonwebtoken'
 import { hash, compare } from 'bcrypt'
 
-const router = express.Router()
+import { appDataSource } from '../data-source';
+import { User } from '../entities/user.entity';
 
 dotenv.config()
+const router = express.Router()
 const accessSecret = process.env.ACCESS_TOKEN_SECRET as Secret
 const refreshSecret = process.env.REFRESH_TOKEN_SECRET as Secret
 
 router.route('/login').post(async (req, res, next) => {
     const { email, password } = req.body;
-
 
     const existingUser = await appDataSource.getRepository(User).findOneBy({ email: email })
 
@@ -23,29 +22,62 @@ router.route('/login').post(async (req, res, next) => {
     }
 
     compare(password, existingUser.password, function(err, result) {
-        if (!result) {
-            const error = Error("Wrong details provided.");
+        if (result === true) {
+            const accessToken = sign({ userId: existingUser.id, email: existingUser.email }, accessSecret, { expiresIn: "30m" });
+
+            const refreshToken = sign({  userId: existingUser.id, email: existingUser.email }, refreshSecret, { expiresIn: '15d' });
+        
+            res.cookie("accessToken", accessToken, { httpOnly: true, 
+                sameSite: 'none', secure: true, 
+                maxAge: 30 * 60 * 1000
+            });
+        
+            res.cookie("refreshToken", refreshToken, { httpOnly: true, 
+              sameSite: 'none', secure: true, 
+              maxAge: 24 * 60 * 60 * 1000 * 15
+            });
+        
+            res.status(200).json({"message": "Success"})
+        } else {
+            res.status(401).json({"message": "Incorrect Password"})
+        }
+    });
+})
+
+
+router.route('/signup').post(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const newUser = new User()
+    hash(password, 10, async function(err, hash) {
+        newUser.email = email
+        newUser.password = hash
+
+        try {
+            await appDataSource.getRepository(User).save(newUser)
+        } catch {
+            const error = new Error("Error! Something went wrong.");
             return next(error);
         };
     });
-    
-    const accessToken = sign({ userId: existingUser.id, email: existingUser.email }, accessSecret, { expiresIn: "30m" });
 
-    const refreshToken = sign({  userId: existingUser.id, email: existingUser.email }, refreshSecret, { expiresIn: '15d' });
+    const accessToken = sign({ userId: newUser.id, email: newUser.email }, accessSecret, { expiresIn: "30m" });
 
+    const refreshToken = sign({  userId: newUser.id, email: newUser.email }, refreshSecret, { expiresIn: '15d' });
+  
     res.cookie("accessToken", accessToken, { httpOnly: true, 
         sameSite: 'none', secure: true, 
         maxAge: 30 * 60 * 1000
     });
 
     res.cookie("refreshToken", refreshToken, { httpOnly: true, 
-      sameSite: 'none', secure: true, 
-      maxAge: 24 * 60 * 60 * 1000 * 15
+        sameSite: 'none', secure: true, 
+        maxAge: 24 * 60 * 60 * 1000 * 15
     });
 
-
-    res.status(200).json({"message": "Success"})
+    res.status(200).json({"message": "Success"});
 })
+
 
 router.route('/refresh').post(async (req, res) => {
     if (req.cookies?.refreshToken) {
@@ -79,64 +111,5 @@ router.route('/refresh').post(async (req, res) => {
     }
 });
 
-
-router.route('/signup').post(async (req, res, next) => {
-    const { email, password } = req.body;
-
-    const newUser = new User()
-
-    newUser.email = email
-    hash(password, 10, async function(err, hash) {
-        newUser.password = hash
-        try {
-            // Creating the new user in DB.
-            await appDataSource.getRepository(User).save(newUser)
-        } catch {
-            const error = new Error("Error! Something went wrong.");
-            return next(error);
-        };
-    });
-
-
-
-    let accessToken;
-    try {
-        // Creating jwt token
-        accessToken = sign(
-          { userId: newUser.id, email: newUser.email },
-          accessSecret, // TODO: Move secret key to .env
-          { expiresIn: "30m" }
-        );
-
-    } catch (err) {
-        const error = new Error("Error! Something went wrong.");
-        return next(error);
-    };
-
-    let refreshToken;
-    try {
-        refreshToken = sign(
-            {  userId: newUser.id, email: newUser.email },
-            refreshSecret, 
-            { expiresIn: '15d' }
-        );
-    } catch (err) {
-            const error = new Error("Error! Something went wrong.");
-            return next(error);
-    };
-
-    // Assigning refresh token in http-only cookie 
-    res.cookie("refreshToken", refreshToken, { httpOnly: true, 
-        sameSite: 'none', secure: true, 
-        maxAge: 24 * 60 * 60 * 1000 * 15
-    });
-  
-      res.cookie("accessToken", accessToken, { httpOnly: true, 
-          sameSite: 'none', secure: true, 
-          maxAge: 30 * 60 * 1000
-    });
-
-    res.status(200).json({"message": "Success"});
-})
 
 module.exports = router
